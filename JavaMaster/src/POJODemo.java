83ce064576bc03fc603b33ae997a1e523743cf75
kishan870 <narasimhakishan392@gmail.com>
2024-08-10 19:59:54 -0400

    Demonstrate the usage of POJO class (Student), toString() method

---
+++ JavaMaster/src/POJODemo.java
@@ -0,0 +1,19 @@
+public class POJODemo {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++) {
+            Student student = new Student("S92300" +i,
+                    switch(i) {
+                case 1 -> "Mary";
+                case 2 -> "Carol";
+                case 3 -> "Tim";
+                case 4 -> "Harry";
+                case 5 -> "Lisa";
+                default -> "Anonymous";
+                    },
+                    "03/11/1996",
+                    "Java.Masterclass");
+
+            System.out.println(student);
+        }
+    }
+}

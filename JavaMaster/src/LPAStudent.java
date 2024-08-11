238547ff9b77ae521173bfcd0e109b296eeab574
kishan870 <narasimhakishan392@gmail.com>
2024-08-10 20:12:31 -0400

    Demonstrate the usage of Record in java

---
+++ JavaMaster/src/LPAStudent.java
@@ -0,0 +1,6 @@
+
+//When you declare a record, the constructors, getters, setters, and toString methods are implicitly defined
+//No need to define any methods externally
+//LPA -> Learn Programming Academy
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+}

0b378037ed186e659a8f10b72f9c51669f1dcf26
kishan870 <narasimhakishan392@gmail.com>
2024-09-02 21:23:53 -0400

    Sets Challenge

--- JavaMaster/SetOperationsChallenge/src/SetsChallenge.java
+++ JavaMaster/SetOperationsChallenge/src/SetsChallenge.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SetsChallenge {
     public static void main(String[] args) {
@@ -23,6 +21,25 @@ public class SetsChallenge {
 
         Set<Task> unassignedTasks = getDifference(tasks, assignedTasks);
         Main.sortAndPrint("Unassigned Tasks", unassignedTasks);
+
+        Set<Task> overlap = getUnion(List.of(
+                getIntersection(annsTasks, bobsTasks),
+                getIntersection(bobsTasks, carolsTasks),
+                getIntersection(annsTasks, carolsTasks)
+        ));
+
+        Main.sortAndPrint("Assigned to multiples", overlap);
+
+        List<Task> overlapping = new ArrayList<>();
+        for(Set<Task> set : sets) {
+            Set<Task> dupes = getIntersection(set, overlap);
+            overlapping.addAll(dupes);
+        }
+
+        Comparator<Task> sortByPriority = Comparator.comparing(Task::getPriority);
+        Comparator<Task> priorityNatural = sortByPriority
+                .thenComparing(Comparator.naturalOrder());
+        Main.sortAndPrint("Overlapping", overlapping, priorityNatural);
 
     }
 

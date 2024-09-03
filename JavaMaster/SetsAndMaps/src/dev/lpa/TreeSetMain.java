8e8f4138a1662cd52d37678bc466866b7ec8bc45
kishan870 <narasimhakishan392@gmail.com>
2024-09-03 09:16:04 -0400

    TreeSet Intro

---
+++ JavaMaster/SetsAndMaps/src/dev/lpa/TreeSetMain.java
@@ -0,0 +1,95 @@
+package dev.lpa;
+
+import java.util.*;
+
+public class TreeSetMain {
+    public static void main(String[] args) {
+
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> emails = ContactData.getData("email");
+
+        //NavigableSet<Contact> sorted = new TreeSet<>(phones);
+
+        Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
+        NavigableSet<Contact> sorted = new TreeSet<>(mySort);
+        sorted.addAll(phones);
+        sorted.forEach(System.out::println);
+
+        //Strings have natural sort order
+        NavigableSet<String> justNames = new TreeSet<>();
+        phones.forEach(s -> justNames.add(s.getName()));
+        System.out.println(justNames);
+
+        NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
+        fullSet.addAll(emails);
+        fullSet.forEach(System.out::println);
+
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+        fullList.sort(sorted.comparator());
+        System.out.println("-".repeat(50));
+        fullList.forEach(System.out::println);
+
+        Contact min = Collections.min(fullSet, fullSet.comparator());
+        Contact max = Collections.max(fullSet, fullSet.comparator());
+
+        Contact first = fullSet.first();
+        Contact last = fullSet.last();
+
+        System.out.println("-".repeat(50));
+        System.out.printf("min = %s, first = %s %n", min.getName(), first.getName());
+        System.out.printf("max = %s, last = %s %n", max.getName(), last.getName());
+
+        NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
+        System.out.println("First Element = " + copiedSet.pollFirst());
+        System.out.println("Last Element = " + copiedSet.pollLast());
+        copiedSet.forEach(System.out::println);
+        System.out.println("-".repeat(50));
+
+        Contact daffy = new Contact("Daffy Duck");
+        Contact daisy = new Contact("Daisy Duck");
+        Contact snoopy = new Contact("Snoopy");
+        Contact archie = new Contact("Archie");
+
+        for(Contact c: List.of(daffy, daisy, last, snoopy)) {
+            System.out.printf("ceiling(%s)=%s%n", c.getName(), fullSet.ceiling(c));
+            System.out.printf("higher(%s)=%s%n", c.getName(), fullSet.higher(c));
+        }
+        System.out.println("-".repeat(50));
+
+        for(Contact c: List.of(daffy, daisy, first, archie)) {
+            System.out.printf("floor(%s)=%s%n", c.getName(), fullSet.floor(c));
+            System.out.printf("lower(%s)=%s%n", c.getName(), fullSet.lower(c));
+        }
+        System.out.println("-".repeat(50));
+
+        NavigableSet<Contact> descendingSet = fullSet.descendingSet();
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(50));
+
+        //Any changes to descendingSet will also get reflected in fullSet
+        Contact lastContact = descendingSet.pollLast();
+        System.out.println("Removed contact: " + lastContact);
+        System.out.println("-".repeat(50) + "Descending Set");
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(50) + "Original Set");
+        fullSet.forEach(System.out::println);
+
+        //Head Set and Tail Set methods
+        System.out.println("-".repeat(50));
+        Contact marion = new Contact("Maid Marion");
+
+        var headSet = fullSet.headSet(marion);
+        headSet.forEach(System.out::println);
+        System.out.println("-".repeat(50));
+
+        var tailSet = fullSet.tailSet(marion);
+        tailSet.forEach(System.out::println);
+        System.out.println("-".repeat(50));
+
+        Contact linus = new Contact("Linus Van Pelt");
+        var subSet = fullSet.subSet(linus, marion);
+        subSet.forEach(System.out::println);
+        System.out.println("-".repeat(50));
+    }
+}

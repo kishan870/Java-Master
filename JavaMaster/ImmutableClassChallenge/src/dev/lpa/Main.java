ed840a7e636c75f245dba65569d7b58802f0e62f
kishan870 <narasimhakishan392@gmail.com>
2024-09-18 16:41:15 -0400

    Immutable classes challenge

--- JavaMaster/ImmutableClassChallenge/src/dev/lpa/Main.java
+++ JavaMaster/ImmutableClassChallenge/src/dev/lpa/Main.java
@@ -1,7 +1,9 @@
 package dev.lpa;
 
+import dev.bank.Bank;
 import dev.bank.BankAccount;
 import dev.bank.BankCustomer;
+import dev.dto.Transaction;
 
 import java.util.List;
 
@@ -9,15 +11,49 @@ public class Main {
     public static void main(String[] args) {
 //        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING, 500);
 //        System.out.println(account);
+//
+//        BankCustomer joe = new BankCustomer("Joe", 500.00,
+//                10000.00);
+//        System.out.println(joe);
 
-        BankCustomer joe = new BankCustomer("Joe", 500.00,
+        Bank bank = new Bank(3214567);
+        bank.addCustomer("Joe", 500.00,
                 10000.00);
+
+        BankCustomer joe = bank.getCustomer("000000010000000");
         System.out.println(joe);
+
+        if(bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING,
+                35)) {
+            System.out.println("Transaction successful");
+            System.out.println(joe);
+        }
+
+        if(bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING,
+                -535)) {
+            System.out.println("Transaction successful");
+            System.out.println(joe);
+        }
+
+        BankAccount checking = joe.getAccount(BankAccount.AccountType.CHECKING);
+        var transactions = checking.getTransactions();
+        transactions.forEach((k, v) -> System.out.println(k + " : " + v));
+
+        System.out.println("-".repeat(20));
+
+//        for(var tx: transactions.values()) {
+//            tx.setCustomerId(2);
+//            tx.setAmount(10000);
+//        }
+
+        joe.getAccount(BankAccount.AccountType.CHECKING).getTransactions().clear();
+        joe.getAccount(BankAccount.AccountType.CHECKING).getTransactions()
+                .forEach((k, v) -> System.out.println(k + " : " + v));
 
         //Checking if Joe account data is immutable
-        List<BankAccount> accounts = joe.getAccounts();
-        accounts.clear();
-        System.out.println(joe);
+//        List<BankAccount> accounts = joe.getAccounts();
+//        accounts.clear();
+//        System.out.println(joe);
 
        // accounts.add(new BankAccount(BankAccount.AccountType.CHECKING, 150000.00));
        // System.out.println(joe);

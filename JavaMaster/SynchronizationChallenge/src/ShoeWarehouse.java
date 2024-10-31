1b7d12e21989047dfe20a4c16b7686bcb6fbe9ce
kishan870 <narasimhakishan392@gmail.com>
2024-10-31 19:54:48 -0400

    Synchronization challenge

---
+++ JavaMaster/SynchronizationChallenge/src/ShoeWarehouse.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+record Order(long orderId, String item, int qty) {
+}
+
+public class ShoeWarehouse {
+
+    private List<Order> shippingItems;
+    public final static String[] PRODUCT_LIST =
+            {"Running Shoes", "Sandals", "Boots", "Slippers", "High heels"};
+
+    public ShoeWarehouse() {
+        this.shippingItems = new ArrayList<>();
+    }
+
+    public synchronized void receiveOrder(Order item) {
+
+        while (shippingItems.size() > 20) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        shippingItems.add(item);
+        System.out.println("Incoming: " + item);
+        notifyAll();
+    }
+
+    public synchronized Order fulfillOrder() {
+
+        while (shippingItems.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Order item = shippingItems.remove(0);
+        System.out.println(Thread.currentThread().getName() + " Fulfilled " + item);
+        notifyAll();
+        return item;
+    }
+}
